25b1c9eecd186dabb9598692fd5139e6530b8165
Sai-Charan-Thumuluru <cherry.learning.art63@gmail>
2025-02-14 15:15:10 +0530

    Converted to module JS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
-const express = require('express');
+import express from 'express'
+import { config } from 'dotenv';
+
 const app = express();
-const port = 3000;
+config()
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`<h1> Hello world </h1>`);
