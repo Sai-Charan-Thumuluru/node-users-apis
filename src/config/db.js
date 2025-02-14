83041d4df99da95426da20e484944fe0b6d45ab3
Sai-Charan-Thumuluru <cherry.learning.art63@gmail>
2025-02-14 20:06:46 +0530

    Configured MongoDB connection

---
+++ src/config/db.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const mongoDBUri = process.env.MONGODB_URI;
+        await mongoose.connect(mongoDBUri);
+    } catch (error) {
+        console.log(`Error connecting to MongoDB: ${error}`);
+        process.exit(1);
+    }
+}
+
+const disconnectDB = async () => {
+    try {
+        await mongoose.disconnect();
+    } catch (error) {
+        console.log(`Error disconnecting from MongoDB: ${error}`);
+    }
+}
+
+(() => {
+    mongoose.connection.on('connecting', () => {
+        console.log('Connecting to MongoDB');
+    });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB');
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Disconnected from MongoDB');
+    });
+
+    mongoose.connection.on('error', (error) => {
+        console.log(`MongoDB connection error: ${error}`);
+    });
+})();
+
+export { connectDB, disconnectDB };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { config } from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
 
 const app = express();
 config()
 const port = process.env.PORT || 3000;
+await connectDB();
 
 app.get('/', (req, res) => {
     res.send(`<h1> Hello world </h1>`);
@@ -12,3 +14,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
 });
+
+const gracefulShutDown = async (signal) => {
+    try {
+        await disconnectDB();
+        console.log(`Recieved ${signal}, shutting down gracefully`);
+        process.exit(0);
+    } catch (error) {
+        console.log(`Error during graceful shutdown ${error}`);
+        process.exit(1);
+    }
+}
+
+process.on('SIGINT', () => gracefulShutDown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutDown('SIGTERM'));
