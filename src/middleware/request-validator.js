f93c362907c76b6f31f3c2ac498a6519041935ac
Sai-Charan-Thumuluru <159702699+Sai-Charan-Thumuluru@users.noreply.github.com>
2025-02-19 20:02:43 +0530

    Improved request validation and error response format

--- src/middleware/request-validator.js
+++ src/middleware/request-validator.js
@@ -5,30 +5,44 @@ const validRegex = `(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+]).*$`;
 const userValidators = [
     body('username')
         .trim()
-        .notEmpty().withMessage('Username should not be blank!')
-        .isLength({ min: 3, max: 12 }).withMessage('Username must contain 3-12 characters!')
-        .matches(validRegex).withMessage('Username must contain numbers, letters and special characters!')
+        .notEmpty().withMessage('Username is required!')
+        .bail()
+        .isLength({ min: 3, max: 12 }).matches(validRegex).withMessage('Username must contain 3-12 characters, numbers, letters and special characters!')
         .escape(),
 
     body('email')
         .trim()
+        .notEmpty().withMessage('Email is required!')
+        .bail()
         .isEmail()
         .normalizeEmail().withMessage('Email is invalid!')
         .escape(),
 
     body('password')
         .trim()
-        .isLength({ min: 6 }).withMessage('Password must contain atleast 6 characters')
-        .matches(validRegex).withMessage('Password must contain numbers, letters and special characters!')
+        .notEmpty().withMessage('Password is required!')
+        .bail()
+        .isLength({ min: 6 }).matches(validRegex).withMessage('Password must contain atleast 6 characters, numbers, letters and special characters!')
         .escape()
 ];
 
 const requestValidator = (req, res, next) => {
     try {
-        const errors = validationResult(req);
-        if (errors.array().length !== 0) {
-            const errorMessage = errors.array()[0].msg ? errors.array()[0].msg : '';
-            return res.status(400).json({ message: `${errorMessage}` });
+        const errors = validationResult(req).array()
+            .filter((error) => error.msg !== "Invalid value");
+
+        const errorResponse = {};
+        errorResponse.message = `Request validation failed.`;
+        errorResponse.validationErrors = errors.map((error) => {
+            return {
+                fieldName: error.path,
+                message: `Invalid input '${error.value}'`,
+                error: error.msg
+            }
+        });
+
+        if (errors.length !== 0) {
+            return res.status(400).json(errorResponse);
         }
         next();
     } catch (error) {
